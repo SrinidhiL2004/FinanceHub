77374578cabfdf62b9ac8640fc972c4d4e688dc3
SrinidhiL <getsrinidhil@gmailcom>
2026-07-01 13:05:45 +0530

    Initial Spring Boot setup with PostgreSQL integration

---
+++ src/test/java/com/srinidhi/financehub/FinancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinidhi.financehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
